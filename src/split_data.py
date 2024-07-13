2859803efac6c88589a67db6882fef9d053b89e1
Rev4s0x <varunrevankar11@gmail.com>
2024-07-13 11:47:53 +0530

    commit-27

--- src/split_data.py
+++ src/split_data.py
@@ -1,5 +1,5 @@
-import os
-import yaml
+#import os
+#import yaml
 import pandas as pd
 import numpy as np
 import argparse
@@ -9,12 +9,12 @@ from sklearn.model_selection import train_test_split
 
 
 def split_data(config_path):
-    config = read_params(config_path)
-    test_data_path = config["split_data"]["test_path"]
-    train_data_path = config["split_data"]["train_path"]
-    raw_data_path = config["load_data"]["raw_data"]
-    split_ratio = config["split_data"]["test_size"]
-    random_state = config["base"]["random_state"]
+    config=read_params(config_path)
+    test_data_path=config["split_data"]["test_path"]
+    train_data_path=config["split_data"]["train_path"]
+    raw_data_path=config["load_data"]["raw_data"]
+    split_ratio=config["split_data"]["test_size"]
+    random_state=config["base"]["random_state"]
     df = pd.read_csv(raw_data_path, sep=",")
     #print(df)
 
@@ -26,7 +26,7 @@ def split_data(config_path):
 
 
 if __name__=="__main__":
-    args = argparse.ArgumentParser()
+    args=argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
-    parsed_args = args.parse_args()
+    parsed_args=args.parse_args()
     split_data(config_path=parsed_args.config)
